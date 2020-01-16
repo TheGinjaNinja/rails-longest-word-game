25f3c666c74e0423f65b140e33cd588533420e74
Jamie Reynolds <jamie.s.a.reynolds@gmail.com>
2020-01-16 21:27:41 +0000

    Progress so far

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,37 @@
 class GamesController < ApplicationController
   def new
-    @letters = [generate_letters(10)]
+    @letters = generate_letters(10)
   end
 
   def score
-    render plain: "Add to DB restaurant '#{params[:name]}' with address '#{params[:address]}'"
+    # check if the letters typed in the form are not in the grid
+    answer = params[:word]
+    answer.chars
+
+    letters = params[:letters]
+    saved_letters = letters.split
+
+    # TODO: You are stuck here, trying to test whether the if statement is working!
+
+    result = answer & saved_letters
+
+    if result.empty?
+      @score = "fail"
+    else
+
+    # if saved_letters.any? { answer }
+    #   @score = "freedoooom!"
+    # else
+    #   @score = "Computer says no..."
+    # end
+
+    # TODO: check if the word is an English word
   end
 
   private
 
   def generate_letters(number)
     charset = Array('A'..'Z')
-    Array.new(number) { charset.sample }.join
+    Array.new(number) { charset.sample }
   end
 end
